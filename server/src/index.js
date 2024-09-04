eee4f6fd32e99a1954f7b3f1a94280e063961eb4
Aaron Dangc <adraging@gmail.com>
2024-09-03 23:44:47 -0700

    learned dotconfig ../ pathing for env variables

--- server/src/index.js
+++ server/src/index.js
@@ -1,14 +1,21 @@
-// server/src/index.js
+// This is the entry point of our backend server
+const path = require('path');
+require('dotenv').config({ path: path.join(__dirname, '../../.env') });
+console.log(process.env.MONGODB_URI);
 const express = require('express');
+
+
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('Hello from the backend!');
+  res.send(`Hello from the backend! ${process.env.MONGODB_URI}`);
 });
 
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+  console.log(`Server running on port localhost:${port}`);
 });
+
+
