2c91210fc2ec33371118926899178e1942664412
Aaron Dangc <adraging@gmail.com>
2024-09-03 20:52:20 -0700

    setting up project structure working on login UI

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,56 @@
 import logo from './logo.svg';
 import './App.css';
 
+import React, { useEffect } from 'react';
+
+
+// Define our views
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/UPDATEIT.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  useEffect(() => {
+    document.title = "Welcome!";
+  },[])
+
+  
+  return (<>
+  <h1 className="text-center mt-5 mb-5">Sign in to continue!</h1>
+  {/* <h2 className="text-center mt-5 mb-5">Login</h2> */}
+
+  {/* Email */}
+  <div class="p-5 mb-5">
+    <label for="exampleFormControlInput1" class="form-label">Email address</label>
+    <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com"></input>
+  </div>
+  {/* Password */}
+  <div class="p-5">
+    <label for="exampleFormControlTextarea1" class="form-label">Password</label>
+    <input type="password" class="form-control" id="exampleFormControlInput1" placeholder="Password"></input>
+  </div>
+  {/* Login and Create Account Button */}
+  <div class="p-5 text-center">
+    <button class="btn btn-primary btn-lg pt-2 pb-2 p-5">Login</button>
+  </div>
+  
+  <div style={{ display: 'flex', alignItems: 'center' ,justifyContent :'center'}} class="text-center">
+      <p style={{ margin: '0 8px 0 0' }}>Don't have an account?</p>
+      <button style={{ padding: '8px 16px', cursor: 'pointer' }} class="btn btn-info">Sign Up</button>
+  </div>
+  
+  
+  </>);
+
+  
 }
+
+function Login(){
+
+}
+
+function SignUp(){
+
+}
+
+
+
+
 
 export default App;
