e9e3597f4475da84228dde3d9815fe130424817b
Aaron Dangc <adraging@gmail.com>
2024-09-10 11:06:02 -0700

    added/update sign up view

--- client/src/Components/Views/login.js
+++ client/src/Components/Views/login.js
@@ -1,79 +1,89 @@
 
-import React, { useState } from 'react';
+import {React,  useState, useEffect}from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import axios from 'axios';
-import { Navigate } from 'react-router-dom';
+// import { Navigate } from 'react-router-dom';
+// import { useEffect } from 'react';
 
 
 function Login() {
-    const [emailOrUsername, setEmailOrUsername] = useState('');
-    const [password, setPassword] = useState('');
-    const [error, setError] = useState('');
-    const navigate = useNavigate();
-  
-    const handleLogin = async (e) => {
-      e.preventDefault();
-      setError(''); // Reset error
-  
-      try {
-        const response = await axios.post('http://localhost:5000/login', {
-          username : emailOrUsername,
-          email : emailOrUsername,
-          password
-        });
-        console.log('Login successful:', response.data);
-        // Handle successful login (e.g., redirect or show a success message)
-        sessionStorage.setItem('token', response.data.token);
-        // Redirect to hub 
-        navigate('/hub');
-        window.location.reload();
-      } catch (err) {
-        console.error('Error during login:', err);
-        setError('Login failed. Please check your credentials.');
-      }
-    };
-  
-    return (
-      <div className="container p-5" style={{ marginTop: '10em', minWidth: '80%' }}>
-        <form onSubmit={handleLogin}>
-          <fieldset>
-            <legend className="text-center fs-1">Sign in to continue!</legend>
-            <div className="p-4">
-              <label htmlFor="emailOrUsername" className="form-label fs-5">Email or Username</label>
-              <input
-                type="text"
-                className="form-control fs-5"
-                id="emailOrUsername"
-                placeholder="Enter your email or username"
-                value={emailOrUsername}
-                onChange={(e) => setEmailOrUsername(e.target.value)}
-                required
-              />
-            </div>
-            <div className="p-4">
-              <label htmlFor="password" className="form-label fs-5">Password</label>
-              <input
-                type="password"
-                className="form-control fs-5"
-                id="password"
-                placeholder="Enter your password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                required
-              />
-            </div>
-            <div className="text-center mt-4 mb-5">
-              <button type="submit" className="fs-2 cta btn btn-primary">Login</button>
-            </div>
-            <div className="text-center mt-4">
-              <p className="fs-3">Not a user? <Link to="/signup" className="text-primary" id="signup">Sign up</Link></p>
-            </div>
+
+
+  useEffect(() => {
+    document.title = "Login | Memory Lane";
+    window.scrollTo(0, 0);
+  },[])
+
+  const [emailOrUsername, setEmailOrUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+  const navigate = useNavigate();
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    setError(''); // Reset error
+
+    try {
+      const response = await axios.post('http://localhost:5000/login', {
+        username : emailOrUsername,
+        email : emailOrUsername,
+        password
+      });
+      console.log('Login successful:', response.data);
+      // Handle successful login (e.g., redirect or show a success message)
+      sessionStorage.setItem('token', response.data.token);
+      // Redirect to hub 
+      navigate('/hub');
+      window.location.reload();
+    } catch (err) {
+      console.error('Error during login:', err);
+      setError('Login failed. Please check your credentials.');
+    }
+  };
+
+  return (
+    <div className="container p-5" style={{ marginTop: '10em', minWidth: '80%' }}>
+      <form onSubmit={handleLogin}>
+        <fieldset>  
+          <legend className="text-center fs-1">Sign in to continue!</legend>
+          <div className="p-4">
+            <label htmlFor="emailOrUsername" className="form-label fs-5">Email or Username</label>
+            <input
+              type="text"
+              className="form-control fs-5"
+              id="emailOrUsername"
+              placeholder="Enter your email or username"
+              title = "Please enter your email or username"
+              value={emailOrUsername}
+              onChange={(e) => setEmailOrUsername(e.target.value)}
+              required
+            />
+          </div>
+          <div className="p-4">
+            <label htmlFor="password" className="form-label fs-5">Password</label>
+            <input
+              type="password"
+              className="form-control fs-5"
+              id="password"
+              placeholder="Enter your password"
+              title = "Please enter your password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </div>
+          <div className="text-center mt-4 mb-5">
+            <button type="submit" className="fs-2 cta btn btn-primary">Login</button>
+          </div>
+          <div className="text-center mt-4">
+            <p className="fs-3">Not a user? <Link to="/signup" className="text-primary" id="below-link">Sign up</Link></p>
+          </div>
             {error && <div className="text-center text-danger">{error}</div>}
-          </fieldset>
-        </form>
-      </div>
-    );
-  }
+        </fieldset>
+      </form>
+    </div>
+  );
+}
   
 
   export default Login;
--- client/src/Components/Views/signup.js
+++ client/src/Components/Views/signup.js
@@ -1,10 +1,108 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import axios from 'axios';
+
+
 
 function SignUp(){
-    useEffect(() => {},[])
-    return(<>
-      <h1 className="text-center"style = {{marginTop:3 + "em"}}>SignUp</h1>
-      </>)
+
+  useEffect(() => {
+    document.title = "Sign Up | Memory Lane";
+    window.scrollTo(0, 0);
+  },[])
+
+  const [email,setEmail] = useState('');
+  const [username,setUsername] = useState('');
+  const [password,setPassword] = useState('');
+  const [error,setError] = useState('');
+  const navigate = useNavigate();
+
+  const handleSignUp = async (e) => {
+    // e.preventDefault();
+    e.preventDefault();
+    setError(''); // Reset error
+
+    try {
+      const response = await axios.post('http://localhost:5000/register', {
+        'email' : email,
+        'username' : username,
+        'password' : password
+      });
+
+      console.log('Signup successful:', response.data);
+      alert('Signup successful');
+      setEmail('');
+      setUsername('');
+      setPassword('');
+      navigate('/login');
+    } catch (err) {
+      console.error('Error during signup:', err);
+      // setError('Signup failed. Please check your credentials.');
+      setError(err.response?.data?.message || 'Signup failed. Please check your credentials.');
+    }
+  }
+
+
+
+  return(
+  <>
+    <div className="container p-5" style={{marginTop :'10em', minWidth:'80%'}}>
+      <form onSubmit = {handleSignUp}>
+          <fieldset>
+            <legend className="text-center fs-1">Let's Get Started!</legend>
+            <div className="p-2">
+              <label htmlFor="email" className="form-label fs-5">Email</label>
+              <input
+                type="text"
+                className="form-control fs-5"
+                id="email"
+                placeholder="Enter your email"
+                title = "Enter a valid email address"
+                value={email}
+                onChange = {(e) => setEmail(e.target.value)} 
+                required
+              />
+            </div>
+            <div className="p-2">
+              <label htmlFor="username" className="form-label fs-5">Username</label>
+              <input
+                type="text"
+                className="form-control fs-5"
+                id="username"
+                placeholder="Enter your username"
+                title="3-20 chars, letters, numbers, '.' or '_', no consecutive '.' or '_'" 
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                required
+              />
+            </div>
+            <div className="p-2">
+              <label htmlFor="password" className="form-label fs-5">Password</label>
+              <input
+                type="password"
+                className="form-control fs-5"
+                id="password"
+                placeholder="Enter your password" 
+                title="At least 8 characters 1 uppercase, 1 lowercase, 1 number and 1 special character"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
+            </div>
+            <div className="text-center mt-4 ">
+              <button type="submit" className="fs-2 cta btn btn-primary">Signup</button>
+            </div>
+            <div className="text-center mt-4">
+              <p className="fs-3">Already a user? <Link to="/login" className="text-primary" id="below-link">Login</Link></p>
+            </div>
+            {error && <div className="text-center text-danger">{error}</div>}
+          </fieldset>
+      </form>
+    </div>
+
+
+
+  </>)
   
 }
 
