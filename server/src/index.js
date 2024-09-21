5922e06a489124ca604d74135ebce049fa0df11b
Aaron Dangc <adraging@gmail.com>
2024-09-20 19:51:40 -0700

    edit env var , added production links to backend

--- server/src/index.js
+++ server/src/index.js
@@ -92,7 +92,7 @@ if (require.main === module){
 
 app.use(express.json());
 app.use(cors({
-  origin: 'http://localhost:3000', // Your frontend URL
+  origin: 'https://adanglol.github.io/memory-lane/', // Your frontend URL
   credentials: true
 }));
 app.use(cookieParser());
@@ -291,7 +291,7 @@ app.get('/memories', authenticateJWT, async (req, res) => {
       id: memory._id,
       title: memory.title,
       description: memory.description,
-      link: `http://localhost:5000/memories/${memory._id}`,
+      link: `https://memory-lane-t90a.onrender.com//memories/${memory._id}`,
       createdAt: memory.createdAt
     }));
 
@@ -316,7 +316,7 @@ app.get('/memories/:id', authenticateJWT, async (req, res) => {
       description: memory.description,
       createdAt: memory.createdAt,
       audio: {
-        url: `http://localhost:5000/memories/${memory._id}/audio`, // URL to fetch the audio
+        url: `https://memory-lane-t90a.onrender.com/memories/${memory._id}/audio`, // URL to fetch the audio
         contentType: memory.contentType // MIME type for the audio
       }
     });
@@ -344,9 +344,10 @@ app.get('/memories/:id/audio', authenticateJWT, async (req, res) => {
 
 
 // Only listen the server if we're not in a test environment
-if (process.env.NODE_ENV !== 'test') {
+if (process.env.NODE_ENV !== 'development' || process.env.NODE_ENV === 'production') {
     app.listen(port, () => {
       console.log(`Server is running on http://localhost:${port}`);
+
     });
 }
 
